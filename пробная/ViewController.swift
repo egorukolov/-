6fe09b4979ea9caa399d127195654bc8918f7646
Egor Ukolov <ukolovegor@yandex.ru>
2021-12-09 23:47:10 +0700

    Настройка логики основного экрана

--- пробная/ViewController.swift
+++ пробная/ViewController.swift
@@ -15,10 +15,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .red
+        showTextButton.layer.cornerRadius = 10
     }
 
     @IBAction func showTextButton(_ sender: UIButton) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showTextButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showTextButton.setTitle("Show Text", for: .normal)
+        }
     }
     
 }
